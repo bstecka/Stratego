29cf619e2691d605f6a71bc26d33d1ce750f2964
negacja <bstecka@gmail.com>
2018-05-11 18:04:18 +0200

    Prioritize Middle

--- src/com/company/PlayerMinMax.java
+++ src/com/company/PlayerMinMax.java
@@ -5,10 +5,12 @@ import javafx.util.Pair;
 public class PlayerMinMax extends Player {
 
     private int depth;
+    private int type;
 
     public PlayerMinMax(char symbol, int depth) {
         super(symbol);
         this.depth = depth;
+        this.type = 2;
     }
 
     @Override
@@ -21,12 +23,47 @@ public class PlayerMinMax extends Player {
         return false;
     }
 
+    private int getBestWorstValue(Game gameState) {
+        int score = 0;
+        switch (type) {
+            case 1:
+                score = getScoreDifference(gameState);
+                break;
+            case 2:
+                int factoredScoreDifference = gameState.getBoard().length * gameState.getBoard().length * 10 * getScoreDifference(gameState);
+                int factoredMiddleDiff = prioritizeMiddleEvaluation(gameState, this);
+                factoredMiddleDiff -= prioritizeMiddleEvaluation(gameState, gameState.getOpponent(this));
+                score = factoredScoreDifference + factoredMiddleDiff;
+                break;
+        }
+        return score;
+    }
+
+    private int getScoreDifference(Game gameState) {
+        return this.getScore() - gameState.getOpponent(this).getScore();
+    }
+
+    private int prioritizeMiddleEvaluation(Game gameState, Player player) {
+        int score = 0;
+        Square[][] board = gameState.getBoard();
+        int middle = board.length / 2;
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board.length; j++){
+                if (board[i][j].getPlayer() == player) { //abs(środek - indeks) ma być najbliższy zeru
+                    score += middle - Math.abs(middle - j);
+                    score += middle - Math.abs(middle - i);
+                }
+            }
+        }
+        return score;
+    }
+
     private Pair<Square, Integer> minMax(Square[] availableMoves, Game gameState, Player player, int depth) {
         int i, bestWorstValue;
         Square bestMove = null;
         for(i = 0; i < availableMoves.length && availableMoves[i].isMarked(); i++){}
         if (depth == 0 || i >= availableMoves.length) {
-            bestWorstValue = this.getScore() - gameState.getOpponent(this).getScore();
+            bestWorstValue = getBestWorstValue(gameState);
         }
         else {
             if (player == this)
