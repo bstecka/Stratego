15c580cea9aba0ace4c7de0548c43abd8dc048f2
negacja <bstecka@gmail.com>
2018-05-11 18:34:25 +0200

    Prioritize Middle Eval

--- src/com/company/PlayerMinMax.java
+++ src/com/company/PlayerMinMax.java
@@ -30,11 +30,9 @@ public class PlayerMinMax extends Player {
                 score = getScoreDifference(gameState);
                 break;
             case 2:
-                int factoredScoreDifference = gameState.getBoard().length * gameState.getBoard().length * 100 * getScoreDifference(gameState);
+                int factoredScoreDifference = 100 * getScoreDifference(gameState);
                 int factoredMiddleDiff = prioritizeMiddleEvaluation(gameState, this);
                 factoredMiddleDiff -= prioritizeMiddleEvaluation(gameState, gameState.getOpponent(this));
-                if (factoredMiddleDiff > factoredScoreDifference)
-                    factoredMiddleDiff = factoredScoreDifference - 1;
                 score = factoredScoreDifference + factoredMiddleDiff;
                 break;
         }
@@ -45,9 +43,8 @@ public class PlayerMinMax extends Player {
         return this.getScore() - gameState.getOpponent(this).getScore();
     }
 
-    //consider changing to floats?
     private int prioritizeMiddleEvaluation(Game gameState, Player player) {
-        int score = 0;
+        int score = 0, count = 0;
         Square[][] board = gameState.getBoard();
         int middle = board.length / 2;
         for (int i = 0; i < board.length; i++){
@@ -55,10 +52,11 @@ public class PlayerMinMax extends Player {
                 if (board[i][j].getPlayer() == player) { //abs(środek - indeks) ma być najbliższy zeru
                     score += middle - Math.abs(middle - j);
                     score += middle - Math.abs(middle - i);
+                    count++;
                 }
             }
         }
-        return score;
+        return score/count + 1;
     }
 
     private Pair<Square, Integer> minMax(Square[] availableMoves, Game gameState, Player player, int depth) {
